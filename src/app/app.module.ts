3e06944e7e5ba43f56afbc7ee13eb4974f78d8fe
Avinash Sankineni <sankineni@acheron-tech.com>
2022-12-21 09:57:25 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavBarComponent,
+    BookListComponent,
+    BookDetailsComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule,
